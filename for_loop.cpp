6f01c74bce41d29bd6a243ff269cbdbe19046de3
Aryan Pathak <aryanpathak327@gmail.com>
2026-02-10 22:54:55 +0530

    Added the File
    Adding the loops file

---
+++ for_loop.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // for(int i=0;i<=10;i++){
+    //     cout<<"Striver"<<endl;
+    // }
+
+    // int i = 1;
+    // while(i<=5){
+    //     cout<<"Striver"<<endl;
+    //     i = i+1;
+    // }
+    // return 0;
+}
