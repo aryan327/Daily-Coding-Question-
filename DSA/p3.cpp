8d050a95f08888c6391655542da689d14636b8be
Aryan Pathak <aryanpathak327@gmail.com>
2026-02-14 00:46:52 +0530

    learned Something new
    tried out new patterns

---
+++ DSA/p3.cpp
@@ -0,0 +1,37 @@
+// print the patterns which looks like 
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n, i, j;
+//     cin>>n;
+//     for(i=1;i<=n;i++){
+//         for(j=1;j<=i;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// print the Pattern 
+// 1
+// 22
+// 333
+// 4444
+// 5555
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
