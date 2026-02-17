ca3018f168e92e31dce405beef433983011f301c
Aryan Pathak <aryanpathak327@gmail.com>
2026-02-17 22:08:33 +0530

    new pattern added
    pattern added

---
+++ DSA/p10.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=(5-i)+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
